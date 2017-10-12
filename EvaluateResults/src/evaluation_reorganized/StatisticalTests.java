2287f7080b5986a2cb8456c2a451ad12c5674a70
Marcelo Oliveira <marcatam.o@gmail.com>
2017-10-12 09:39:18 -0300

    Testing cases

---
+++ EvaluateResults/src/evaluation_reorganized/StatisticalTests.java
@@ -0,0 +1,57 @@
+package evaluation_reorganized;
+
+public class StatisticalTests {
+	
+	 public static void main(String[] args) {
+
+//	    	double a = 4.0;
+//	    	double b = 25.0;
+//	    	double c = 9.0;
+//	    	printCalc(a, b, c);
+//	    	printCalcRoot(Math.sqrt(a), Math.sqrt(b), Math.sqrt(c));
+	    	
+//	    	double a = 4.0;
+//	    	double b = 9.0;
+//	    	double c = 25.0;
+//	    	printCalc(a, b, c);
+//	    	printCalcRoot(Math.sqrt(a), Math.sqrt(b), Math.sqrt(c));
+	    	
+//	    	double a = 9.0;
+//	    	double b = 4.0;
+//	    	double c = 25.0;
+//	    	printCalc(a, b, c);
+//	    	printCalcRoot(Math.sqrt(a), Math.sqrt(b), Math.sqrt(c));
+	    	
+//	    	double a = 9.0;
+//	    	double b = 25.0;
+//	    	double c = 4.0;
+//	    	printCalc(a, b, c);
+//		    printCalcRoot(Math.sqrt(a), Math.sqrt(b), Math.sqrt(c));
+	    	
+//	    	double a = 25.0;
+//	    	double b = 4.0;
+//	    	double c = 9.0;
+//	    	printCalc(a, b, c);
+//		    printCalcRoot(Math.sqrt(a), Math.sqrt(b), Math.sqrt(c));
+	    	
+//	    	double a = 25.0;
+//	    	double b = 9.0;
+//	    	double c = 4.0;
+//	    	printCalc(a, b, c);
+//		    printCalcRoot(Math.sqrt(a), Math.sqrt(b), Math.sqrt(c));
+	    	
+		}
+
+		private static void printCalc(double a, double b, double c) {
+			System.out.println(b / a);
+	    	System.out.println(Math.sqrt(b / a));
+	    	System.out.println(Math.sqrt(1 - (c / a)));
+		}
+		
+		private static void printCalcRoot(double a, double b, double c) {
+			System.out.println(b / a);
+	    	System.out.println(b / a);
+	    	System.out.println(1 - (c / a));
+		}
+
+}
